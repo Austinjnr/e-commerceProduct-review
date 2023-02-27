c820421b87c0b81a2122a2db7460b94a733ad42a
austinjnr <austinobonyo@gmail.com>
2023-02-27 11:08:14 +0300

    edits migration reviews

--- db/migrate/20230227075559_create_reviews.rb
+++ db/migrate/20230227075559_create_reviews.rb
@@ -1,8 +1,12 @@
 class CreateReviews < ActiveRecord::Migration[7.0]
+
   def change
     create_table :reviews do |t|
       t.integer :star_rating
       t.string :comment
+      t.integer :product_id
+      t.integer :user_id
     end
+    
   end
 end

36ee67f8048902a3e83260189e5b4628309cb5ef
austinjnr <austinobonyo@gmail.com>
2023-02-27 18:22:47 +0300

    fixed bug on leave review method

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ActiveRecord::Base
     has_many :users, through: :reviews
 
     def leave_review(user, star_rating, comment)
-        Review.create(star_rating: star_rating, comment: comment, user_id: user.id, product_id: product.id)
+        Review.create(star_rating: star_rating, comment: comment, user_id: user.id, product_id: self.id)
     end
 
     def print_all_reviews
